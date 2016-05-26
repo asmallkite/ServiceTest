1a600ed3e3b6a0da45146e43626d9adeba5442f5
Li Zheng <lixiaozheng.good@163.com>
2016-05-26 23:30:14 +0800

    complete start and destory service

--- app/src/main/java/com/example/a10648/servicetest/MainActivity.java
+++ app/src/main/java/com/example/a10648/servicetest/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.a10648.servicetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.start_service)
+    Button startService;
+    @BindView(R.id.end_service)
+    Button endService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.start_service, R.id.end_service})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this,MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.end_service:
+                Intent stopService = new Intent(this, MyService.class);
+                stopService(stopService);
+                break;
+        }
     }
 }
